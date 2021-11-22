120756183b329a380c06f0bbe4628b48e7a7aa15
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2021-11-21 20:01:09 -0600

    functions fully defined to clean data, need to expect more random cases, dataframe only contains number values or NaN to represent missing data

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import re
+
 url = "https://www.rottentomatoes.com/m/dune_2021/reviews"
 website_url = requests.get("https://www.rottentomatoes.com/m/dune_2021/reviews")
 
@@ -30,18 +31,70 @@ hasNextPage as to find if there are more pages,
 
 small_review = soup.find_all("div", {"class": "small subtle review-link"})  # Score and href
 
-score_review = soup.find_all(string=re.compile("Original Score"))  # Score
+# score_review = soup.find_all(string=re.compile("Original Score"))  # Score
 
-def find_char(string, character):
+
+def find_char(string: str, character: str):
     return True if character in string else False
 
 
-review = review_with_missing_scores = []
+def add_nan_for_missing_scores(score_list: list):
+    for score in score_list:
+        pass
+
+
+review = []  # for calculations
+review_with_missing_scores = []  # for dataframe
+count = 1
+
 for i in small_review:
     non_split_review = i.get_text(strip=True)
     review_with_missing_scores.append(non_split_review)
-    if find_char(non_split_review, "|"):
-        review.append(non_split_review)
 
-for i in review:
-    print(i)
+    if find_char(non_split_review, "|"):
+        review.append((count, non_split_review.split(" ")[4]))
+
+    count += 1
+
+full_review_without_missing_scores = []
+
+for i in review_with_missing_scores:
+    append_string = i
+    if not find_char(i, "|"):
+        append_string += "| Original Score: NaN"
+    full_review_without_missing_scores.append(append_string.split(": ")[1])
+
+
+full_review_without_letters = []
+
+def letter_score_to_number_score_converter(string_list: list):
+    # instad of if checks, find different way to check the value of score_to_append
+    for i in string_list:
+        score_to_append = i
+        if score_to_append != "NaN" and score_to_append.isalpha():
+            if score_to_append == "A" or score_to_append == "a":
+                score_to_append = "9.5/10"
+            elif score_to_append == "B" or score_to_append == "b":
+                score_to_append = "8.5/10"
+            elif score_to_append == "C" or score_to_append == "c":
+                score_to_append = "7.5/10"
+            elif score_to_append == "D" or score_to_append == "d":
+                score_to_append = "6.5/10"
+
+        full_review_without_letters.append(score_to_append)
+
+
+
+
+def number_score_to_common_score_converter():
+    pass
+
+
+df = pd.DataFrame(full_review_without_missing_scores, columns=["Score"])
+
+# print(df)
+
+letter_score_to_number_score_converter(full_review_without_missing_scores)
+
+df_new = pd.DataFrame(full_review_without_letters, columns=['Score'])
+print(df_new)
