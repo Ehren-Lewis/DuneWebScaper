c5300f79fc7684498f04352149e2e390da65d7bc
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2021-11-23 17:47:29 -0600

    cleaned data further, code is now in functions, all data is out of 10

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ brainstorming area to see what I may want to webscrape, the intentions, etc...
 
 
 """
+"""
+Second iteration: show all reviews on the website
+Third iteration: filter review by the types,
+fourth iterations: search by movie that you want to see the results for
+
+feature: have a default way they want to show the data, then ask if they want a specific
+way to see it (only the ones that are plausible with the dataset
+
+"""
+
+
+
 import pandas
 from bs4 import BeautifulSoup
 import requests
@@ -31,6 +43,7 @@ hasNextPage as to find if there are more pages,
 
 small_review = soup.find_all("div", {"class": "small subtle review-link"})  # Score and href
 
+
 # score_review = soup.find_all(string=re.compile("Original Score"))  # Score
 
 
@@ -38,63 +51,77 @@ def find_char(string: str, character: str):
     return True if character in string else False
 
 
-def add_nan_for_missing_scores(score_list: list):
-    for score in score_list:
-        pass
+def create_review_lists():
+    score_list = []
+    count = 1
+    for i in small_review:
+        non_split_review = i.get_text(strip=True)
+        score_list.append(non_split_review)
+        count += 1
+    return score_list
 
 
-review = []  # for calculations
-review_with_missing_scores = []  # for dataframe
-count = 1
+def nan_score_adder(missing_score_list: list):
+    full_review_without_missing_scores = []
+    for i in missing_score_list:
+        append_string = i
+        if not find_char(i, "|"):
+            append_string += "| Original Score: NaN"
+        full_review_without_missing_scores.append(append_string.split(": ")[1])
+    return full_review_without_missing_scores
 
-for i in small_review:
-    non_split_review = i.get_text(strip=True)
-    review_with_missing_scores.append(non_split_review)
-
-    if find_char(non_split_review, "|"):
-        review.append((count, non_split_review.split(" ")[4]))
-
-    count += 1
-
-full_review_without_missing_scores = []
-
-for i in review_with_missing_scores:
-    append_string = i
-    if not find_char(i, "|"):
-        append_string += "| Original Score: NaN"
-    full_review_without_missing_scores.append(append_string.split(": ")[1])
-
-
-full_review_without_letters = []
 
 def letter_score_to_number_score_converter(string_list: list):
-    # instad of if checks, find different way to check the value of score_to_append
+    letter_to_number_dict = {"A+": "9.8/10", "a+": "9.8/10", "A": "9.5/10", "a": "9.5", "A-": "9.1/10", "a-": "9.1/10",
+                             "B+": "8.8/10", "b+": "8.8/10", "B": "8.5/10", "b": "8.5/10", "B-": "8.1/10",
+                             "b-": "8.1/10",
+                             "C+": "7.8/10", "c+": "7.8/10", "C": "7.5/10", "c": "7.5/10", "C-": "7.1/10",
+                             "c-": "7.1/10",
+                             "D+": "6.8/10", "d+": "6.8/10", "D": "6.5/10", "d": "6.5/10", "D-": "6.1/10",
+                             "d-": "6.1/10",
+                             "E": "5.5/10", "e": "5.5/10"}
+    full_review_without_letters = []
     for i in string_list:
         score_to_append = i
         if score_to_append != "NaN" and score_to_append.isalpha():
-            if score_to_append == "A" or score_to_append == "a":
-                score_to_append = "9.5/10"
-            elif score_to_append == "B" or score_to_append == "b":
-                score_to_append = "8.5/10"
-            elif score_to_append == "C" or score_to_append == "c":
-                score_to_append = "7.5/10"
-            elif score_to_append == "D" or score_to_append == "d":
-                score_to_append = "6.5/10"
+            score_to_append = letter_to_number_dict[score_to_append]
 
         full_review_without_letters.append(score_to_append)
+    return full_review_without_letters
+
+
+def number_score_to_common_score_converter(number_list):
+    common_denominator_list = []
+    x = None
+    '''
+    3    x
+    - =  - : first numb* 10 / second numb = x
+    4   10
+        '''
+
+    for numb_str in number_list:
+        if numb_str == "NaN":
+            common_denominator_list.append(numb_str)
+            continue
+        number_test = numb_str.split("/")
+        first_number = float(number_test[0])
+        second_number = float(number_test[1])
+
+        if second_number == 10:
+            continue
+        x = (first_number * 10) / second_number
+        x = round(x, 1)
+        common_denominator_list.append(f"{x}/10")
+    return common_denominator_list
+
+
+review_with_missing_scores = create_review_lists()
+review_without_missing_scores = nan_score_adder(review_with_missing_scores)
+non_equal_number_review = letter_score_to_number_score_converter(review_without_missing_scores)
+common_denom_scores_with_nan = number_score_to_common_score_converter(non_equal_number_review)
+
+df = pd.DataFrame(common_denom_scores_with_nan, columns=['Score'])
+print(df)
 
 
 
-
-def number_score_to_common_score_converter():
-    pass
-
-
-df = pd.DataFrame(full_review_without_missing_scores, columns=["Score"])
-
-# print(df)
-
-letter_score_to_number_score_converter(full_review_without_missing_scores)
-
-df_new = pd.DataFrame(full_review_without_letters, columns=['Score'])
-print(df_new)
